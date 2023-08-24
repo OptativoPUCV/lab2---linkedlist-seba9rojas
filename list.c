785d0ed10af7be3ed2f1529f881a950a33dc9fc4
Replit user <>
2023-08-24 02:36:52 +0000

    new try

--- list.c
+++ list.c
@@ -49,7 +49,7 @@ void* firstList(List * list){
 void* nextList(List * list){
     
     if(list->head!=NULL){
-      return (void*)list->head->next;
+      return (void*)(list->head->next);
     }
     else{
       return NULL;
